61477684aa58fff0852c23405044d8d685de8315
artcreatezart <abbie@Abbies-MBP.hub>
2024-07-27 14:02:53 +1200

    first commit

---
+++ src/routes/Links.jsx
@@ -0,0 +1,12 @@
+import {Routes, Route} from 'react-router-dom'
+// Page/Component imports
+import Home from '../components/pages/Home'
+const Links = () => {
+  return (
+    <Routes>
+      <Route exact path='/' element={<Home/>}/>
+    </Routes>
+  )
+}
+
+export default Links
