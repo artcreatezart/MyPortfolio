57985214c6c2d8c3ea411024a54c363780a52ed3
artcreatezart <abbie@Abbies-MacBook-Pro.local>
2024-09-16 14:23:21 +1200

    added more content into skills

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -171,6 +171,52 @@ const portfolioProjects = [
   },
 ]
 
+const hardSkills = [
+  {
+    id: 1,
+    name: 'Analysis',
+    image: '/img/skillsArrayImg/analysis.png',
+  },
+  {
+    id: 2,
+    name: 'Team Work',
+    image: '/img/skillsArrayImg/teamwork-skill.png',
+  },
+  {
+    id: 3,
+    name: 'Communication',
+    image: '/img/skillsArrayImg/communication.png',
+  },
+  {
+    id: 4,
+    name: 'Problem Solving',
+    image: '/img/skillsArrayImg/problem-skil.png',
+  }
+]
+
+const softSkills = [
+  {
+    id: 1,
+    name: 'Analysis',
+    image: '/img/skillsArrayImg/analysis.png',
+  },
+  {
+    id: 2,
+    name: 'Team Work',
+    image: '/img/skillsArrayImg/teamwork-skill.png',
+  },
+  {
+    id: 3,
+    name: 'Communication',
+    image: '/img/skillsArrayImg/communication.png',
+  },
+  {
+    id: 4,
+    name: 'Problem Solving',
+    image: '/img/skillsArrayImg/problem-skil.png',
+  }
+]
+
 
 const Home = () => {
 
@@ -243,17 +289,28 @@ const Home = () => {
       {/* START OF SKILL SECTION */}
       <section id='skillSection'>
         <h1>Skills</h1>
-        <div className='skill-display'>
-          <div className='hard-skills-container'>
+          <div className='skills-container'>
+              {hardSkills.map((hardSkill) => (
+              <div key={hardSkill.id} className="skills-card">
+                <img src={hardSkill.image} alt={`${hardSkill.name} logo`} className='skills-card-img'/>
+                <h2 className='skills-card-name'>{hardSkill.name}</h2>
+                
+              </div>
+            ))}
+            
             <div className='skills-layout-container'></div>
           </div>
-          <div className='line-divider'/>
-          <div className='soft-skills-container'>
-            <div className='skills-layout-container'></div>
+          <div className='horizantal-line'/>
+          <div className='skills-container'>
+          {softSkills.map((softSkill) => (
+              <div key={softSkill.id} className="skills-card">
+                <img src={softSkill.image} alt={`${softSkill.name}logo`} className='skills-card-img'/>
+                <h2 className='skills-card-name'>{softSkill.name}</h2>
+                
+              </div>
+            ))}
           </div>
           <div className='skill-character-img'/>
-
-        </div>
         
         
       </section>
