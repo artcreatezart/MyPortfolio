964c2908ba5d91597c16b6459f11e832fb61d294
artcreatezart <abbie@Abbies-MBP.hub>
2024-09-23 02:45:21 +1200

    added more info into the project array

--- src/components/sections/projects/Projects.jsx
+++ src/components/sections/projects/Projects.jsx
@@ -48,7 +48,7 @@ export const portfolioProjects = [
         id: 3,
         name: 'Lord of the Fries ',
         mockupImage1: '/img/projectArrayImg/lotfmockup.webp',
-        type: 'Web Development',
+        type: ['Web Development', 'Ux Design'],
         image2: '/img/projectArrayImg/lotf2.png',
         image3: '/img/projectArrayImg/lotf3.png',
         image4: '/img/projectArrayImg/lotf4.png',
@@ -68,7 +68,7 @@ export const portfolioProjects = [
         id: 4,
         name: 'Five Nights At Freddys Website',
         mockupImage1: '/img/projectArrayImg/fnafmockup.webp',
-        type: 'Web Development',
+        type: ['Web Development', 'Ux Design'],
         image2: '/img/projectArrayImg/fnaf2.png',
         image3: '/img/projectArrayImg/fnaf3.png',
         image4: '/img/projectArrayImg/fnaf4.png',
@@ -76,8 +76,8 @@ export const portfolioProjects = [
         image6: '/img/projectArrayImg/fnaf6.png',
         date: '2023',
         credit: '',
-        aboutdescription: 'This was one of the first full websites I coded. This website promoting what was a upcoming film Five Nights At Freddys allows users to checkout an overview of the film, watch a trailer from the site, learn more about the animatronics, checkout film posters by clicking on cameras, and reading about what the film is based off and its origin.',
-        processDescription: ' ',
+        aboutdescription: 'This was one of the first full websites I coded. This website promoting what was a upcoming film Five Nights At Freddys allows users to checkout an overview of the film, watch a trailer from the site, learn more about the animatronics, checkout film posters by clicking on cameras, and reading about what the film is based off and its origin. For this project one thing i looked into was cursor trends and i thought it would be fun to try creating a dynamic cursor. So this website deatures a flashlight cursor that allows you to interact and discover elements throughout the website.',
+        processDescription: 'I started off doing thorough research of SWOT analysis, cursor trends, moodboard, style tile, and user testing. After anyalising pre-production and making the appropriate needed changes and considerations. I started coding, first off focusing getting all the content and basic needs and linking into the html file. Then editing layout and style of those elements in css before getting the dynamic cursor to work from Javascript. This was my first time experiencing Javascript it wasnt needed for this project but i am glad i got to check it out when i did. ',
         github: 'https://github.com/artcreatezart/Five-Nights-at-Freddys-Film-Website---Project',
         vercel: 'https://five-nights-at-freddys-film-website-project.vercel.app/',
         tools: 'Figma - Indesign - VS Code',
@@ -88,7 +88,7 @@ export const portfolioProjects = [
         id: 5,
         name: 'Accomadation Website',
         mockupImage1: '/img/projectArrayImg/acommockup.webp',
-        type: 'Web Development',
+        type: ['Web Development', 'Ux Design'],
         image2: '/img/projectArrayImg/acom2.png',
         image3: '/img/projectArrayImg/acom3.png',
         image4: '/img/projectArrayImg/acom4.png',
@@ -96,13 +96,13 @@ export const portfolioProjects = [
         image6: '/img/projectArrayImg/acom6.png',
         date: '2024',
         credit: '',
-        aboutdescription: 'blah blah blah',
-        processDescription: '',
+        aboutdescription: 'This accomadation webapp was a application users can use to view accomadation around nz and also view nearby animal attractions. To encourage users to get outside and promote well being while also supporting local animal sanctuarys. This webapp allows users to input information into a form for accomadation and then takes them to a filtered accomadation page. This webapp doesnt have full functionailty across all elements such as animal attraction maps but the main focus was to get the main navigation complete and user friendly. It also allows user to input if they want a meal plann and att the end adds the total price together and information for user to view and confirm. ',
+        processDescription: 'For this project pre production was done, from inital research, anaylsis of competitior websites, moodboard, header set, style tile and a prototype created form wireframes, low fis and hifis. After this and looking into how the ocde will be broken up and tackeled I started to code. Starting with basic html and content, moved to getting functionailty workign then proccedding to do some of the css. I did not get as much as I wouldve liked to but im happy with where i did get it in the end. ',
         github: 'https://github.com/artcreatezart/Accomadation-App',
         vercel: 'https://accomadation-app.vercel.app/',
-        tools: '',
-        hardSkills: '',
-        softSkills: '',
+        tools: 'Vs Code - Figma - Indesign - Photoshop',
+        hardSkills: 'Gulp - HTML - SCSS - JS ',
+        softSkills: 'Problem Solving - Time Management',
     },
     {
         id: 6,
@@ -137,12 +137,12 @@ export const portfolioProjects = [
         image6: '/img/projectArrayImg/bro6.png',
       date: '2023',
       credit: '',
-      aboutdescription: '',
-      processDescription: '',
+      aboutdescription: 'This project was made for tourists to see scultures around wellington, a few cafe stops that are worthwhile and a example of a route visitors could follow to see all the sculptures. For each sculpture a blurb about is provided. For one of the blurb includes info and bts photos from a artist i managed to get in contact with and got a amazing experience to be able to connect with other artists and learn more about the community.',
+      processDescription: 'The process started with working out what the problem and solution was, to create the target audience and what the main purpose of the brochure would be. The came the research of compeitior brouchures, typography, color, graphics, information and Maori Values considerations. After anaylising all this information and gathering a direction for the design i started crating wireframes, gathering feedback and using analysed feedback to influence what should be chnaged or considered in the developed idea. Repeated this process of create and feedback a few times until i got to a finale design and tested with success. Then came the printing test on different paper and testing the folds of brouchure and making edits where needed. Then after many tests and back and forth i got the final result which i was really pleased about.',
       github: 'https://github.com/artcreatezart',
       vercel: 'https://vercel.com/abbies-projects-93142625',
-      tools: '',
-      hardSkills: 'Photoshop - Illustrator',
+      tools: 'Photoshop - Email - Illustrator ',
+      hardSkills: 'Photoshop - Illustrator - Indesign',
       softSkills: 'Creativity',
     },
     {
@@ -157,13 +157,13 @@ export const portfolioProjects = [
         image6: '/img/projectArrayImg/box6.png',
       date: '2023',
       credit: '',
-      aboutdescription: '',
-      processDescription: '',
+      aboutdescription: 'This project was to create packaging for a Adderstone necklace froma fictional world called "Acient Magis Bride". This packaging was to reflect the preciousness of the stone while also showing the earthy and magical elements of this product. To evoke what this stone necklace was, was done through not only the graphic design but also the structual design of the packaging and how elements fitted together.',
+      processDescription: 'For this packaging design, I researched all about the typography, graphic, colour, packaging/structural elements from the fictional world aswell as the modern world. I also looked into how resources would be collected ethically from this fictional world and how material are processed and used in the final packaging to reflect if theyre would be any long term negative affects on the enviroment. After doing all the research, I looked into wireframes, and designs of not only the graphics but also the strucutral side of the packaging. Alot of testing, feedback analysing, adjustments and developments lead to the end success of this product. I also was really excited to be able to incoporate alot of hand crafted elements to this packaging as this fictional world was based with magic and alot of hand crafted tools I really wanted to be able to show that throught this design. So i created a stamp the shop would use to brand their logo onto the final packaging, a handwritten note from the forager who gathered the stone and details about that specific stone for the product. Aswell as a sewn strip of fabric to wrap the necklace and a sewn pillow of sort for the necklace to sit in the box.',
       github: 'https://github.com/artcreatezart',
       vercel: 'https://vercel.com/abbies-projects-93142625',
-      tools: '',
-      hardSkills: '',
-      softSkills: 'Creativity',
+      tools: 'Sewing Machine - Calligraphy Pen',
+      hardSkills: 'Photoshop - Indesign - Sewing - Calligraphy',
+      softSkills: 'Creativity - Time Management',
     },
   ]  
 
@@ -190,10 +190,11 @@ const Projects = () => {
 
   // Filter the projects based on selected filters
   const filteredProjects = portfolioProjects.filter((project) => {
-    const matchesType = selectedType === '' || project.type === selectedType;
+    const matchesType = selectedType === '' || 
+        (Array.isArray(project.type) ? project.type.includes(selectedType) : project.type === selectedType);
     const matchesYear = selectedYear === '' || project.date === selectedYear;
     return matchesType && matchesYear;
-  });
+});
 
   return (
     
