c32512cace87fd970dec14db123a826e4f68b47b
artcreatezart <abbie@Abbies-MBP.hub>
2025-01-15 15:39:52 +1300

    having issues with email js setting up template but form is set up and created contact form jsx

--- src/components/sections/contact/ContactForm.jsx
+++ src/components/sections/contact/ContactForm.jsx
@@ -3,18 +3,54 @@ import toast, {Toaster} from 'react-hot-toast';
 import emailjs from 'emailjs-com';
 
 const SERVICE_ID = import.meta.env.VITE_EMAIL_JS_SERVICE_ID
+const TEMPLATE_ID = import.meta.env.VITE_EMAIL_JS_TEMPLATE_ID
+const USER_ID = import.meta.env.VITE_EMAIL_JS_USER_ID 
 
 const ContactForm = () => {
-   
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+    });
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setFormData({...formData, [name]: value});
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs 
+            .send(
+                SERVICE_ID,
+                TEMPLATE_ID,
+                formData,
+                USER_ID
+            )
+            .then(
+                (response) => {
+                    toast.success('Message Sent successfully! Thank you for your message :)')
+                    setFormData({name: '', email: '', subject: '', message: ''});
+                },
+                (error) => {
+                    toast.error('Failed to send message. Please try again')
+                }
+            );
+    }
 
   return (
     <div>
-      <form >
+        <Toaster position='top-center'/>
+      <form onSubmit={handleSubmit}>
 
         <label>Name:
             <input
                 type='text'
                 name='name'
+                value={formData.name}
+                onChange={handleChange}
                 required
             />
         </label>
@@ -23,7 +59,8 @@ const ContactForm = () => {
             <input
                 type='text'
                 name='email'
-                
+                value={formData.email}
+                onChange={handleChange}
                 required
             />
         </label>
@@ -32,7 +69,8 @@ const ContactForm = () => {
             <input
                 type='text'
                 name='subject'
-              
+                value={formData.subject}
+                onChange={handleChange}
                 required
             />
         </label>
@@ -40,13 +78,14 @@ const ContactForm = () => {
         <label>Message:
             <textarea 
                 name='message'
-                
+                value={formData.message}
+                onChange={handleChange}
                 required >
 
             </textarea>
         </label>
 
-        <button >Send a Message</button>
+        <button type='submit'>Send a Message</button>
 
       </form>
     </div>
